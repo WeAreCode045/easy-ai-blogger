43e4f2a264ecd0bef13445267a3e84cc13493115
WeAreCode045 <maurice@code045.nl>
2025-07-23 23:13:31 +0200

    Bump version to 1.0.1 in plugin header

--- easy-ai-blogger.php
+++ easy-ai-blogger.php
@@ -1,7 +1,7 @@
 <?php
 /* Plugin Name: Easy AI Blogger
  * Description: A WordPress plugin to enhance blogging with AI capabilities.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: WeAreCode045
  * Author URI: https://github.com/WeAreCode045
  * License: GPL2
