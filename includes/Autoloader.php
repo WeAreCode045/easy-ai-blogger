95f3eb79c8a27485759fe6c3a38bedad44a6f9cf
WeAreCode045 <maurice@code045.nl>
2025-07-23 23:35:25 +0200

    Refactor Autoloader class method parameters and variable names for consistency

--- includes/Autoloader.php
+++ includes/Autoloader.php
@@ -5,17 +5,17 @@ class Autoloader {
     public static function register() {
         spl_autoload_register([self::class, 'autoload']);
     }
-
-    public static function autoload($class) {
+    public static function autoload($className) {
         // Only autoload EasyAiBlogger classes
-        if (strpos($class, 'EasyAiBlogger\\') !== 0) {
+        if (strpos($className, 'EasyAiBlogger\\') !== 0) {
             return;
         }
-        $base_dir = dirname(__DIR__) . '/';
-        $relative_class = substr($class, strlen('EasyAiBlogger\\'));
-        $file = $base_dir . str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $relative_class) . '.php';
+        $baseDir = dirname(__DIR__) . '/';
+        $relativeClass = substr($className, strlen('EasyAiBlogger\\'));
+        $file = $baseDir . str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $relativeClass) . '.php';
         if (file_exists($file)) {
             require_once $file;
         }
     }
 }
+Autoloader::register();
